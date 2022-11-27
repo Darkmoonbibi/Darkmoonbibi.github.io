93c1788539aa1974b9da53f6c2a7be3594430c30
Darkmoonbibi <Dendivn323@gmail.com>
2022-11-27 15:07:19 +0700

    ád
    ád

--- js/bill.js
+++ js/bill.js
@@ -31,15 +31,17 @@ if(biicore.bgMusic){
 			clearInterval(myInterval);
 		}
 	}, 200);
-
+	var isPlay = false;
 	function playPause() {
 		document.getElementsByClassName("bii-player")[0].classList.remove("show-sec");
 	    if (audioPlayer.paused) {
 			audioPlayer.play();
+			isPlay = true;
 			document.getElementById("playerVolumeOff").style.display = "none";
 			document.getElementById("playerVolumeOn").style.display = "block";
 		} else {
 			audioPlayer.pause(); 
+			isPlay = false;	
 			document.getElementById("playerVolumeOff").style.display = "block";
 			document.getElementById("playerVolumeOn").style.display = "none";
 		}
